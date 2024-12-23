e18912ab6655e32274a91d4b9d5235d688f37733
samrat-dotel <samratdotel7264@gmail.com>
2024-12-23 22:06:20 +0545

    add component folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import Todo from './components/Todo'
+import styles from './App.module.css'
+
+const todos = [
+  {
+    title: 'Learn React',
+    description: 'Learn how to build a web app with React',
+  },
+  {
+    title: 'Learn TypeScript',
+    description: 'Learn how to add types to JavaScript',
+  },
+  {
+    title: 'Learn GraphQL',
+    description: 'Learn how to query and mutate data with GraphQL',
+  },
+  {
+    title: 'Learn Node.js',
+    description: 'Learn how to build a server with Node.js'
+  },
+  {
+    title: 'Learn Express',
+    description: 'Learn how to build a web app with Express', 
+  },
+  {
+    title: 'Learn MongoDB',
+    description: 'Learn how to store data with MongoDB',
+  }
+]
 
 function App() {
   return (
     <main>
-      <h1>React App</h1>
+      <h1 className={styles.navbar}>React App</h1>
+
+      {todos.map((todo, index) => {
+        const { title, description } = todo;
+        return <Todo key={index} title={title} description={description} />;
+      })}
     </main>
   )
 }
